ac729cba1a96d95c886ace62e348786722ec1a62
husker19 <71553562+husker19@users.noreply.github.com>
2021-02-07 14:38:40 +0800

    jd2019: enable blur by default

--- product_prop.mk
+++ product_prop.mk
@@ -94,5 +94,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1 \
     debug.sf.latch_unsignaled=1 \
-    debug.sf.disable_backpressure=1 \
-    persist.sys.sf.disable_blurs=1
+    debug.sf.disable_backpressure=1
